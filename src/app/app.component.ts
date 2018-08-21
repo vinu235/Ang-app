fdac59722c53b24a5f034f5c3a7f419ca2c67dc6
CEO235 <nandanikarvinu@gmail.com>
2018-08-21 15:07:14 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css','../../node_modules/bootstrap/dist/css/bootstrap.css']
 })
 export class AppComponent {
-  title = 'Ang-App';
+  title = 'First Angular App in Action...';
+  
 }
